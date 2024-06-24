fe562f4b979de8808fc14a1c23e9dc6080cceea7
“Magali <maguinavarro07@gmail.com>
2024-06-24 20:44:34 -0300

    Creando las funciones guardar personaje y leer personaje

---
+++ PersonajesJson.cs
@@ -0,0 +1,43 @@
+using System.Text.Json.Serialization;
+using System;
+using System.Text.Json;
+using System.IO;
+using PersonajeApi;
+
+public class PersonajesJson{
+     public static void GuardarPersonajes(List<Character> personajes, string nombreArchivo)
+    {
+        try
+        {
+            var opcionesJson = new JsonSerializerOptions { WriteIndented = true };
+            string json = JsonSerializer.Serialize(personajes, opcionesJson);
+            File.WriteAllText(nombreArchivo, json);
+            Console.WriteLine($"Personajes guardados en {nombreArchivo}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error al guardar personajes: {ex.Message}");
+        }
+    }
+
+    public static List<Character> LeerPersonajes(string nombreArchivo)
+    {
+        try
+        {
+            if (!File.Exists(nombreArchivo))
+            {
+                Console.WriteLine($"El archivo {nombreArchivo} no existe.");
+                return new List<Character>();
+            }
+
+            string json = File.ReadAllText(nombreArchivo);
+            List<Character> personajes = JsonSerializer.Deserialize<List<Character>>(json);
+            return personajes ;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error al leer personajes: {ex.Message}");
+            return new List<Character>();
+        }
+    }
+}
