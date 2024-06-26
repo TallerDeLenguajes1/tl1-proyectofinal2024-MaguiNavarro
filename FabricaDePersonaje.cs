8ff2421654b5d81a3862b23163c931d00bf09398
“Magali <maguinavarro07@gmail.com>
2024-06-25 21:27:37 -0300

    Cree las funciones para Crear personaje Aleatorio

---
+++ FabricaDePersonaje.cs
@@ -0,0 +1,33 @@
+using System;
+using PersonajeApi;
+// FabricaDePersonajes.cs
+using System;
+using System.Collections.Generic;
+
+public class FabricaDePersonajes
+{
+    private static readonly List<string> Tipos = new List<string> { "Guerrero", "Mago", "Arquero", "Asesino" };
+    private static readonly List<string> Nombres = new List<string> { "Jon", "Daenerys", "Tyrion", "Arya", "Sansa" };
+    private static readonly List<string> Familias = new List<string> { "Stark", "Targaryen", "Lannister", "Baratheon", "Greyjoy" };
+    private static readonly Random Random = new Random();
+
+    public Character CrearPersonajeAleatorio(string familia)
+    {
+        var tipo = Tipos[Random.Next(Tipos.Count)];
+        var nombre = Nombres[Random.Next(Nombres.Count)];
+        var fechaDeNacimiento = DateTime.Now.AddYears(-Random.Next(0, 301));
+        var edad = DateTime.Now.Year - fechaDeNacimiento.Year;
+
+        return new Character
+        {
+           
+            FirstName = nombre,
+            Family = familia,
+            Velocidad1 = Random.Next(1, 11),
+            Destreza1 = Random.Next(1, 6),
+            Fuerza1 = Random.Next(1, 11),
+            Nivel1 = Random.Next(1, 11),
+            Armadura1 = Random.Next(1, 11)
+        };
+    }
+}
