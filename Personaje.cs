2f0ff22fb5111012027c618f2d6ad4a439c7b599
“Magali <maguinavarro07@gmail.com>
2024-06-25 21:24:45 -0300

    Creo datos y caracteristicas de personaje

--- Personaje.cs
+++ Personaje.cs
@@ -6,6 +6,7 @@ namespace PersonajeApi
 {
     public class Character
 { 
+  //Datos
      [JsonPropertyName("id")]
     public int Id { get; set; }
 
@@ -20,9 +21,25 @@ namespace PersonajeApi
 
      [JsonPropertyName("family")]
     public string Family { get; set; }
+    
+        // Características
+        private int Velocidad;
+       private int Destreza;
+       private int Fuerza;
+       private int Nivel;
+       private int Armadura;
+       private int Salud;
+        public int Fuerza1 { get => Fuerza; set => Fuerza = value; }
+        public int Nivel1 { get => Nivel; set => Nivel = value; }
+        public int Armadura1 { get => Armadura; set => Armadura = value; }
+        public int Salud1 { get => Salud; set => Salud = value; }
+        public int Destreza1 { get => Destreza; set => Destreza = value; }
+        public int Velocidad1 { get => Velocidad; set => Velocidad = value; }
 
- 
-}
+  
+   
+   
+    }
 }
 
 
